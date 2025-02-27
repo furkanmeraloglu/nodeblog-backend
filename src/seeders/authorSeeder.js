7a74f690c8dbf380a23933416fc1ca2aa626d4e3
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-02-28 01:47:06 +0300

    feat: (database) seeder added and dummy data created for authors and posts

---
+++ src/seeders/authorSeeder.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { faker } from '@faker-js/faker';
+import Author from "../models/author.js";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const seedAuthors = async (num) => {
+    const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+    const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+    await mongoose.connect(mongoURI);
+
+    for (let i = 0; i < num; i++) {
+        const hashedPassword = await bcrypt.hash('password', 10);
+        const author = new Author({
+            name: faker.person.fullName(),
+            username: faker.internet.username(),
+            email: faker.internet.email(),
+            password: hashedPassword,
+            avatar: faker.image.avatar(),
+            createdAt: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2024-01-01T00:00:00.000Z' }),
+            updatedAt: faker.date.between({ from: '2022-01-01T00:00:00.000Z', to: '2024-01-01T00:00:00.000Z' })
+        });
+        await author.save();
+    }
+    await mongoose.connection.close();
+};
+
+seedAuthors(30).then(() => {
+    console.log('Seeding complete');
+}).catch((err) => {
+    console.error('Seeding error:', err);
+});
---
+++ src/seeders/postSeeder.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { faker } from '@faker-js/faker';
+import Post from "../models/post.js";
+import Author from "../models/author.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const seedPosts = async (num) => {
+    const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+    const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+    console.log(mongoURI);
+    /*await mongoose.connect(mongoURI);
+    const authors = await Author.find();
+    if (authors.length === 0) {
+        console.error("No authors found. Please seed authors first.");
+        process.exit(1);
+    }
+    for (let i = 0; i < num; i++) {
+        const randomAuthor = authors[Math.floor(Math.random() * authors.length)];
+        const post = new Post({
+            title: faker.book.title(),
+            content: faker.lorem.paragraph({
+                min: 1,
+                max: 3
+            }),
+            authorId: randomAuthor._id,
+            createdAt: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2022-01-01T00:00:00.000Z' }),
+            updatedAt: faker.date.between({ from: '2022-01-01T00:00:00.000Z', to: '2024-01-01T00:00:00.000Z' })
+        });
+        await post.save();
+    }
+    await mongoose.connection.close();*/
+};
+
+seedPosts(20).then(() => {
+    console.log('Seeding complete');
+}).catch((err) => {
+    console.error('Seeding error:', err);
+});
