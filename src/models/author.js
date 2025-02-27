9cd996c834d97dfbbfb038cce60f7057d3a6c397
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-02-27 16:00:18 +0300

    feat: (models) post and author models created

---
+++ src/models/author.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: false,
+    },
+    likedPosts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: false
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+export default Author;
