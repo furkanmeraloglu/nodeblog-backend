0f8114a71235174a43e3b8c52cd7e10c9d7a38b6
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-02-28 10:41:16 +0300

    feat: (request) post service request validations created

--- src/requests/postCreateRequestValidation.js
+++ src/requests/postCreateRequestValidation.js
@@ -1,28 +1,7 @@
 import {body} from "express-validator";
 
-export const validatePostCreateReuqest = [
-    body('name')
-        .optional()
-        .isLength({min: 2})
-        .withMessage('Name must be at least 2 characters'),
-
-    body('username')
-        .optional()
-        .isLength({min: 3})
-        .withMessage('Username must be at least 3 characters'),
-
-    body('email')
-        .optional()
-        .isEmail()
-        .withMessage('Please provide a valid email address'),
-
-    body('password')
-        .optional()
-        .isLength({min: 6})
-        .withMessage('Password must be at least 6 characters'),
-
-    body('avatar')
-        .optional()
-        .isURL()
-        .withMessage('Avatar must be a valid URL'),
+export const validatePostCreateRequest = [
+    body('title').notEmpty().isString().isLength({min: 3}).withMessage('Title must be at least 3 characters'),
+    body('content').notEmpty().isString().isLength({min: 10}).withMessage('Content must be at least 10 characters'),
+    body('authorId').notEmpty().isString().withMessage('The post must be belong to an author'),
 ];
