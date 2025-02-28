fc681e38b62105e70532cc8a8708426583e09115
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-02-28 10:50:01 +0300

    feat: (post-router) endpoints added to posts.js router

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,5 +1,14 @@
 import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import {validatePostCreateRequest} from "../requests/postCreateRequestValidation.js";
+import {validatePostUpdateRequest} from "../requests/postUpdateRequestValidation.js";
 
 const router = express.Router();
+
+router.get('/', getAllPosts);
+router.get('/:postId', getPostById);
+router.post('/', authMiddleware, validatePostCreateRequest, createPost);
+router.patch('/:postId', authMiddleware, validatePostUpdateRequest, updatePost);
+router.delete('/:postId', authMiddleware, deletePost);
 
 export default router;
