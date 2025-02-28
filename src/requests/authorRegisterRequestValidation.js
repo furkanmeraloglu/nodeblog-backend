96570bc5843a859101002302a4db64dd012cbc9a
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-02-28 10:41:47 +0300

    reformat: (request) author service request validations reformatted

--- src/requests/authorRegisterRequestValidation.js
+++ src/requests/authorRegisterRequestValidation.js
@@ -1,8 +1,8 @@
 import { body } from "express-validator";
 
 export const validateAuthorRegisterRequest = [
-    body('name').notEmpty().withMessage('Name is required'),
-    body('username').notEmpty().withMessage('Username is required'),
-    body('email').isEmail().notEmpty().withMessage('Email is required'),
-    body('password').isLength(6).notEmpty().withMessage('Password must be at least 6 characters long'),
+    body('name').notEmpty().isString().withMessage('Name is required'),
+    body('username').notEmpty().isString().withMessage('Username is required'),
+    body('email').isEmail().notEmpty().isString().withMessage('Email is required'),
+    body('password').isLength(6).isString().notEmpty().withMessage('Password must be at least 6 characters long'),
 ];
