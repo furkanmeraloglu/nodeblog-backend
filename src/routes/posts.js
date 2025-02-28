a90bef948a1c99dac37b74de029ff8f664c003e6
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-02-28 11:45:17 +0300

    feat: (post-create) post create controller and service completed

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,13 +3,14 @@ import authMiddleware from "../middleware/authMiddleware.js";
 import {validatePostCreateRequest} from "../requests/postCreateRequestValidation.js";
 import {validatePostUpdateRequest} from "../requests/postUpdateRequestValidation.js";
 import {getPost, getPosts} from "../controllers/post/postReadController.js";
+import {createPost} from "../controllers/post/postCreateController.js";
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.get('/:postId', getPost);
 router.post('/', authMiddleware, validatePostCreateRequest, createPost);
-router.patch('/:postId', authMiddleware, validatePostUpdateRequest, updatePost);
-router.delete('/:postId', authMiddleware, deletePost);
+// router.patch('/:postId', authMiddleware, validatePostUpdateRequest, updatePost);
+// router.delete('/:postId', authMiddleware, deletePost);
 
 export default router;
